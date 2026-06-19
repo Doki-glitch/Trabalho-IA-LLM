7b842ec850477c8db0df57f61ead78c5455a6ada
VINICIUS DOBKE GASPAR DA SILVA <03970285046@senacrs.edu.br>
2026-06-19 11:51:25 -0300

    Finaliza a tela base do projeto

--- Front/rpg/src/App.tsx
+++ Front/rpg/src/App.tsx
@@ -1,120 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      <html lang='pt-br' className='bg-[#F4EFE6] bg-[url(/Borda.svg)] bg-contain bg-no-repeat min-h-screen bg-center'></html>
+      <div className='pt-18 flex flex-col justify-center items-center gap-3'>
+        <div className='w-1/2'>
+          <h1 className='font-marck text-[32px] text-[#2E2923]'>Vossa Majestade, chegou aos meus ouvidos que a situação de seu reino está grave. Como somos de nações aliadas, decidi prestar apoio à vossa mercê. Estou lhe enviando esta carta. Nela está presente um feitiço que irá conjurar do absoluto nada um herói capaz de salvar sua nação. Para realizar o feitiço você precisa mentalizar o herói e descrevê-lo no espaço demarcado abaixo. Quando a descrição estiver completa, toque com a ponta de sua caneta no pequeno círculo mágico ali presente e assim ele aparecerá.</h1>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+        <div className='bg-[#EAE2D4] w-1/2 h-60 border rounded-[20px] outline-[#2E2923] outline-4 flex flex-col'>
+          <textarea placeholder='Descreva seu personagem' className='m-2 font-garamond font-semibold text-[32px] size-full'></textarea>
+          <div className=' flex items-end justify-end'>
+            <button><img src="Botão.svg" alt="" className='size-16.25 cursor-pointer'/></button>
+          </div>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+        <div className='w-1/2 text-start'>
+          <h1 className='font-marck text-[32px] text-[#2E2923]'>-- Merlin V de Camelot</h1>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
+        <div className='w-1/2 flex justify-end items-end'>
+          <img src="Selo.svg" alt="" className='size-28'/>
         </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </div>
     </>
   )
 }
